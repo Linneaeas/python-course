9a2d3a63e16064c0f7a7582a22724037646a70e9
Leas <127832519+Linneaeas@users.noreply.github.com>
2025-04-03 16:12:51 +0200

    notes on %

--- decades.py
+++ decades.py
@@ -2,3 +2,5 @@ age=int(input("How old are you?\n"))
 decades=age//10
 years=age%10
 print("You are "+str(decades)+" decades and "+str(years)+" years old.")
+
+# In Python, the percent sign (%) is used as the modulus operator, which returns the remainder of a division operation. For example, 5 % 2 would return 1, since 5 divided by 2 leaves a remainder of 1.
