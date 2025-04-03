38d0fcdae01ae346a4e74fa77cdb66b6bde4d22f
Leas <127832519+Linneaeas@users.noreply.github.com>
2025-04-03 16:33:49 +0200

    Random computer choice rock, paper, scissor game

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,7 +1,28 @@
+import random
+
 # FIXED COMPUTER CHOICE
 """
 computer_choice = "scissors"
 user_choice = input("Do you want rock, paper or scissors?")
+
+if computer_choice == user_choice:
+    print("TIE")
+elif user_choice=="rock" and computer_choice=="scissors":
+    print("WIN")
+elif user_choice=="paper" and computer_choice=="rock":
+    print("WIN")
+elif user_choice=="scissors" and computer_choice=="print":
+    print("WIN")
+else: 
+    print("You lose, computer wins!")
+"""
+
+# RANDOM COMPUTER CHOICE
+"""
+computer_choice = random.choice(["rock", "paper", "scissors"])
+user_choice = input("Do you want rock, paper or scissors?")
+
+print("Computer choice: ", computer_choice)
 
 if computer_choice == user_choice:
     print("TIE")
