cb2a2ac3d909636e95b9cf1ac8ef75fa456e912b
Leas <127832519+Linneaeas@users.noreply.github.com>
2025-04-03 16:28:34 +0200

    random(module) numbers

---
+++ roll_dice.py
@@ -0,0 +1,17 @@
+import random
+
+#RANDOM NR
+"""
+roll=random.randint(1,6)
+print("The computer rolled a "+ str(roll))
+"""
+
+#GUESS
+"""
+roll=random.randint(1,6)
+guess = int(input("Guess the dice roll:\n"))
+if guess == roll:
+    print("Correct! They rolled a " + str(roll))
+else:
+      print("Wrong! They rolled a " + str(roll))
+"""
