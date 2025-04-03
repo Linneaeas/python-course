0fea7060b947198a2786d513ee01a32c966fcf22
Leas <127832519+Linneaeas@users.noreply.github.com>
2025-04-03 16:10:46 +0200

    if else, elif, or, and, not, bool

---
+++ weather.py
@@ -0,0 +1,61 @@
+
+#IF ELSE
+"""
+temprature=75
+if temprature >80:
+    print("It's too hot!")
+    print("Stay inside!")
+else:
+    print("Enjoy the outdoors")
+"""
+
+#ELIF
+"""
+temprature=95
+if temprature >80:
+    print("It's too hot!")
+    print("Stay inside!")
+elif temprature < 60:
+    print("It's too cold!")
+    print("Stay inside!")
+else:
+    print("Enjoy the outdoors")
+"""
+
+#OR
+"""
+temprature=75
+if temprature >80 or temprature <60:
+    print("Stay inside!")
+else:
+    print("Enjoy the outdoors")
+"""
+
+#AND
+"""
+temprature=75
+forecast="sunny"
+
+if temprature < 80 and forecast !="rainy":
+    print("Go outside!")
+else:
+    print("Stay inside")
+"""
+
+#NOT
+"""
+forecast="rainy"
+if not forecast == "rainy":
+    print("Go outside!")
+else:
+    print("Stay inside")
+"""
+
+#BOOLEAN
+"""
+raining = False
+if not raining:
+    print("Go outside!")
+else:
+    print("Stay inside") 
+"""
